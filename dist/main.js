1b94a2dea07564a3026909f22f352abfb17dc87a
mhmd <ms.mhmdsalah@gmail.com>
2024-05-16 19:14:56 +0300

    in progress

---
+++ dist/main.js
@@ -0,0 +1,19 @@
+"use strict";
+let inputText = document.getElementById("textInput");
+let btn = document.getElementById("btn");
+let textElement = document.getElementById("textEl");
+let inputSpeed = document.getElementById("speed");
+let text;
+let idx = 0;
+let speed = 1000;
+btn.addEventListener("click", () => {
+    if (inputText.value !== "") {
+        text = inputText.value;
+        setInterval(() => {
+            textElement.innerText = text.slice(0, idx);
+            idx++;
+            if (idx == text.length)
+                idx = 0;
+        }, speed);
+    }
+});
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+let inputText = document.getElementById("textInput") as HTMLInputElement
+let btn = document.getElementById("btn") as HTMLElement;
+let textElement = document.getElementById("textEl")
+let inputSpeed = document.getElementById("speed") as HTMLInputElement
+
+let text:string;
+let idx: number = 0;
+let speed: number = 1000 ;
+
+
+
+
+btn.addEventListener("click", () => {
+    if (inputText.value !== "") {
+        text = inputText.value;
+        setInterval(() => {
+            textElement!.innerText = text.slice(0, idx)
+            idx++
+            if(idx == text.length) idx=0
+        },speed)
+    }
+});
